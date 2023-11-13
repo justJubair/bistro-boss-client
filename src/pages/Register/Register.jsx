c0b336c40124340c952540d89e53def26fc28e3e
Jubair <jubair.ahmed7946@gmail.com>
2023-11-13 22:01:11 +0600

    Add: password validation added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form"
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 const Register = () => {
   const [isButtonDisabled, setIsButtonDisabled] = useState(true)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
   useEffect(()=>{
     loadCaptchaEnginge(6)
   },[])
@@ -25,39 +33,42 @@ const Register = () => {
             <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
           </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
-                <input type="text" placeholder="name" name="name" className="input input-bordered" required />
+                <input type="text" placeholder="name"  {...register("name", {required:true})} className="input input-bordered"  />
+                {errors.name && <span className="mt-2 font-medium text-red-600">Name field is required</span>}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">PhotoURL</span>
                 </label>
-                <input type="text" placeholder="https://img.png"
-                name="photo" className="input input-bordered" required />
+                <input type="text" {...register("photo")} placeholder="https://img.png"
+               className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                <input type="email" placeholder="email"  {...register("email")} className="input input-bordered" required />
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-               
+                <input type="password"  {...register("password",{required: true, minLength:6, pattern: /^(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.*\d).+$/})} placeholder="password" className="input input-bordered"  />
+                {errors.password?.type === "required" && <span className="mt-2 font-medium text-red-600">Password is required</span>}
+                {errors.password?.type === "minLength" && <span className="mt-2 font-medium text-red-600">Password must be six characters or longer</span>}
+                {errors.password?.type === "pattern" && <span className="mt-2 font-medium text-red-600">Password must have atleast one capital letter & a special character</span>}
               </div>
               <div className="form-control mt-4">
         <LoadCanvasTemplate/>
         <input onBlur={handleCaptcha} name="captcha" className="input input-bordered" type="text" placeholder="type the captcha above" />
         </div>
         <div className="form-control mt-6">
-          <button disabled={isButtonDisabled} className="btn btn-primary">Login</button>
+          <button disabled={isButtonDisabled} className="btn btn-primary">Register</button>
         </div>
             </form>
             <div className="flex items-center justify-between px-4 pb-2">
