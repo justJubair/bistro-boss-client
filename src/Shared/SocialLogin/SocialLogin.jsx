a2a233da4d6b5a66c49f7859ae481614e03587f0
Jubair <jubair.ahmed7946@gmail.com>
2023-11-16 13:41:01 +0600

    Add: user duplication stopped in the db

--- src/Shared/SocialLogin/SocialLogin.jsx
+++ src/Shared/SocialLogin/SocialLogin.jsx
@@ -2,15 +2,24 @@ import { FcGoogle } from "react-icons/fc";
 import useAuth from "../../hooks/useAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import useAxios from "../../hooks/useAxios";
 const SocialLogin = () => {
     const navigate = useNavigate()
+    const axios = useAxios()
     const {loginGoogle} = useAuth()
     const handleGoogleLogin= ()=>{
         loginGoogle()
         .then(result=>{
-            if(result)
-            toast.success("Logged in successfully")
-            navigate("/")
+            if(result.user){
+                toast.success("Logged in successfully")
+                navigate("/")
+                const name = result?.user?.displayName
+                const email = result?.user?.email
+                const userInfo = {name, email}
+                axios.post("/users", userInfo)
+                .then()
+            }
+
         })
         .catch(error=>{
             toast.error(error.message)
