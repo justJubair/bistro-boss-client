5e81f4615e863894bdba188b76d33df30fa2f2ab
Jubair <jubair.ahmed7946@gmail.com>
2023-11-18 01:36:27 +0600

    Add: delete item as an admin functionality

--- src/pages/Dashboard/ManageItem/ManageTable.jsx
+++ src/pages/Dashboard/ManageItem/ManageTable.jsx
@@ -1,5 +1,54 @@
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 /* eslint-disable react/prop-types */
 const ManageTabel = ({ menus, refetch }) => {
+  const axiosSecure = useAxiosSecure()
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: "btn btn-success",
+      cancelButton: "btn btn-danger",
+    },
+    buttonsStyling: false,
+  });
+  // DELETE items from cart
+  const handleDelete = (_id) => {
+   
+    swalWithBootstrapButtons
+      .fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          axiosSecure.delete(`/menus/${_id}`).then((result) => {
+            if (result.data.deletedCount) {
+              swalWithBootstrapButtons.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+              refetch()
+            }
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire({
+            title: "Cancelled",
+            text: "Your imaginary file is safe :)",
+            icon: "error",
+          });
+        }
+      });
+  };
+
   return (
     <div>
       <div className="overflow-x-auto">
@@ -35,7 +84,7 @@ const ManageTabel = ({ menus, refetch }) => {
                   <button className="btn btn-ghost btn-xs">update</button>
                 </td>
                 <td>
-                  <button className="btn btn-ghost text-red-500 btn-xs">
+                  <button onClick={()=> handleDelete(menu._id)} className="btn btn-ghost text-red-500 btn-xs">
                     Delete
                   </button>
                 </td>
