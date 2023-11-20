b8765e140367501a0f98a1d6ff7f67a41cf61b8b
Jubair <jubair.ahmed7946@gmail.com>
2023-11-20 18:19:58 +0600

    Add: stripe client secret fetched from server

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,8 +1,25 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useCart from "../../../hooks/useCart";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAxios from "../../../hooks/useAxios";
 
 const CheckoutForm = () => {
+    const [error, setError] = useState("")
     const stripe = useStripe()
     const elements = useElements()
+    const axiosSecure = useAxiosSecure()
+    const [clientSecret, setClientSecret] = useState(" ")
+    const [cart] = useCart()
+    const totalPrice = cart?.reduce((total, item)=> total+item.price,0)
+    
+    useEffect(()=>{
+        axiosSecure.post("/create-payment-intent", {price: totalPrice})
+        .then(res=>{
+            console.log(res.data)
+        })
+    },[axiosSecure, totalPrice])
+
 
     const handleSubmit = async e=>{
         e.preventDefault()
@@ -22,8 +39,10 @@ const CheckoutForm = () => {
 
         if(error){
             console.log(error)
+            setError(error.message)
         } else{
             console.log(paymentMethod)
+            setError(" ")
         }
 
     }
@@ -48,6 +67,7 @@ const CheckoutForm = () => {
           <div className="text-center my-6">
           <button className="btn w-full bg-[#D1A054]" type="submit" disabled={!stripe}>Pay</button>
           </div>
+          <p className="text-red-600 font-semibold">{error}</p>
         </form>
     )}
 export default CheckoutForm;
